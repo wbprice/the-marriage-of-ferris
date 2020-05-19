043b6e5de08928b4dd2adef4a898c34255e89f14
Blaine Price <1wbprice@gmail.com>
2020-05-18 23:26:57 -0400

    Updates

--- get_household/src/main.rs
+++ get_household/src/main.rs
@@ -10,41 +10,36 @@ fn main() {
 
 fn handler(request: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
     let path_parameters = request.path_parameters();
-    let uuid : Uuid = Uuid::parse_str(
-        path_parameters.get("id").unwrap()
-    ).unwrap();
+    let id = path_parameters.get("id");
 
-    match HouseholdService::get(uuid) {
-        Ok(household_response) => {
-            match household_response {
+    if let Some(uuid) = Uuid::parse_str(id) {
+        match HouseholdService::get(uuid) {
+            Ok(household_response) => match household_response {
                 Some(record) => {
                     Ok(Response::builder()
                         .header("Access-Control-Allow-Origin", "*")
                         .status(200)
-                        .body(serde_json::to_string(&record)?)
-                        .unwrap()
-                        .body()
-                        .to_string())
-                },
+                        .body(serde_json::to_string(&record)?));
+                }
                 None => {
                     Ok(Response::builder()
                         .header("Access-Control-Allow-Origin", "*")
-                        .status(200)
-                        .body(serde_json::to_string("{}")?)
-                        .unwrap()
-                        .body()
-                        .to_string())
+                        .status(404)
+                        .body());
                 }
+            },
+
+            Err(error) => {
+                Ok(Response::builder()
+                    .header("Access-Control-Allow-Origin", "*")
+                    .status(500)
+                    .body(error.to_string()));
             }
-        },
-        Err(error) => {
-            Ok(Response::builder()
-                .header("Access-Control-Allow-Origin", "*")
-                .status(500)
-                .body(serde_json::to_string("{}")?)
-                .unwrap()
-                .body()
-                .to_string())
         }
+    } else {
+        Ok(Response::builder()
+            .header("Access-Control-Allow-Origin", "*")
+            .status(400)
+            .body());
     }
 }
